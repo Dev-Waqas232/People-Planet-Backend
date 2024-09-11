9e5db19ea4a7ff03b9b617859060542ece2ad30a
Waqas Munir <devwaqas232@gmail.com>
2024-09-11 16:59:03 +0500

    Created Api for updating post

--- controllers/post.ts
+++ controllers/post.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import Post from '../models/post';
 import Comment from '../models/comment';
+import mongoose from 'mongoose';
 
 const createPost = async (req: Request, res: Response) => {
   const { content } = req.body;
@@ -17,4 +18,29 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
-export { createPost };
+const updatePost = async (req: Request, res: Response) => {
+  const { content } = req.body;
+  const { postId } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(postId))
+      return res.status(404).json({ message: 'Post not found', ok: false });
+
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      { content },
+      { new: true, runValidators: true }
+    );
+
+    if (!post)
+      return res.status(404).json({ message: 'Post not found', ok: false });
+
+    res
+      .status(200)
+      .json({ message: 'Post updated successfully', ok: true, data: post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', ok: false });
+  }
+};
+
+export { createPost, updatePost };
