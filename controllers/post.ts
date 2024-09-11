2f75f7ee1d5777d5e1a6c89e4ef7a9223b73d6e5
Waqas Munir <devwaqas232@gmail.com>
2024-09-11 17:02:31 +0500

    Created Api for deleting post

--- controllers/post.ts
+++ controllers/post.ts
@@ -43,4 +43,25 @@ const updatePost = async (req: Request, res: Response) => {
   }
 };
 
-export { createPost, updatePost };
+const deletePost = async (req: Request, res: Response) => {
+  const { postId } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(postId))
+      return res.status(404).json({ message: 'Post not found', ok: false });
+
+    const post = await Post.findByIdAndDelete(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found', ok: false });
+    }
+
+    res
+      .status(200)
+      .json({ message: 'Post deleted Successfully', ok: true, data: post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', ok: false });
+  }
+};
+
+export { createPost, updatePost, deletePost };
